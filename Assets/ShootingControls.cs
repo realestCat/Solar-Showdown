f68c7ecd5c4a009a01997b6229c50885ad21cd1d
SehunH1 <sjheo01@gmail.com>
2024-05-06 18:58:57 -0700

    Edited Shooting Control
    Made character rotatable and shoot from rotated directions. Added timer between each shot.

--- Assets/ShootingControls.cs
+++ Assets/ShootingControls.cs
@@ -6,23 +6,43 @@ public class ShootingControl : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public Transform shootPoint;
-
     public float projectileSpeed = 10f;
+    public float shootCooldown = 0.5f;
+    public float shootTimer = 0f;
 
     void Update()
     {
+        shootTimer -= Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        RotateTowardsMouse(mouseWorldPosition);
+
+        if (Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0f)
         {
             Shoot();
+            shootTimer = shootCooldown;
         }
+    }
+
+    void RotateTowardsMouse(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - transform.position;
+        direction.z = 0f;
+
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        shootPoint.rotation = rotation;
+        transform.rotation = rotation;
     }
 
     void Shoot()
     {
         GameObject projectile = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-        rb.velocity = shootPoint.up * projectileSpeed; // Use shootPoint.up for 2D
+        rb.velocity = shootPoint.up * projectileSpeed;
+        rb.gravityScale = 0f;
     }
 
 }
